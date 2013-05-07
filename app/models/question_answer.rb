d9e8ec91e2a45b69311c860d5275a24437c2cc9e
Angelo+Nikias <Angelo+Nikias@appacademy.io>
2013-05-07 14:06:55 -0400

    one kind of validation works for users and same questions

--- app/models/question_answer.rb
+++ app/models/question_answer.rb
@@ -3,9 +3,25 @@ class QuestionAnswer < ActiveRecord::Base
 
   validates :user_id, :presence => true
   validates :answer_id, :presence => true
+  validate :unique_qa
+
+  # validates :user_id, :uniqueness => { :scope => :answer_id }
+
+
 
   belongs_to :user
   belongs_to :answer
 
   has_one :question, :through => :answer
+
+
+  def unique_qa
+    QuestionAnswer.all.each do |qa_pair|
+      if qa_pair.user_id == self.user_id && qa_pair.question.id == self.question.id
+        return errors.add(:user_id, "cannot answer same question")
+      end
+    end
+  end
+
+
 end
